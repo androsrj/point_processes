432d1a7a5272fc5f4690a2eb0939f66cf77b4a92
Robert Jacob Andros <androsrj@r1.stat.tamu.edu>
2024-03-15 21:40:39 -0500

    trace plots 5k iterations

--- functions/parallel.R
+++ functions/parallel.R
@@ -1,7 +1,7 @@
 # Helper function to run subsets in parallel for D-and-C
 pp_parallel <- function(i) {
   x <<- cbind(x1 = lansing$x[subsets[[i]]], x2 = lansing$y[subsets[[i]]])
-  langevin_pp(x = x, t, N, K, starting, step = step_sizes, nIter = 1000, nBurn = 100, nThin = 2)
+  langevin_pp(x = x, t, N, K, starting, step = step_sizes, nIter = 5000, nBurn = 500, nThin = 2)
 }
 
 
